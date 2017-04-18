13d56389352607ac6336acbd795abff83e508626
Ryosuke Sato <ryosuke.sato@mixi.co.jp>
2017-04-18 20:12:43 +0900

    Add chai & sinon

--- tests/HelloWorld.spec.ts
+++ tests/HelloWorld.spec.ts
@@ -1,6 +1,10 @@
 import HelloWorld from '../HelloWorld';
+import * as chai from "chai";  // TypeScriptなので、こんな感じ。ES6だといらない。 
+
+const expect = chai.expect;
 
 describe("HelloWorld React component", () => {
     it("test", () => {
+        expect(1).to.a('number');
     });
 });
